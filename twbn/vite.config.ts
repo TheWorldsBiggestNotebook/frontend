0cca3f823734ad43dca9d0c2cc43b22b9ae8f85c
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2026-04-27 18:43:13 +0300

    feat: added linting and a pre-commit hook

--- twbn/vite.config.ts
+++ twbn/vite.config.ts
@@ -1,19 +1,15 @@
-import babel from "@rolldown/plugin-babel"
-import tailwindcss from "@tailwindcss/vite"
-import react, { reactCompilerPreset } from "@vitejs/plugin-react"
-import path from "path"
-import { defineConfig } from "vite"
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    babel({ presets: [reactCompilerPreset()] }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [babel({ presets: [reactCompilerPreset()] }), react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
